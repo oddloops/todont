167e7513486fb406bc93d887eab7283be6f25b53
Vinci Wu <wu.vinci00@gmail.com>
2023-07-14 23:53:38 -0400

    Added React useState states to Form component and App.js

--- frontend/todont/src/App.js
+++ frontend/todont/src/App.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
-  // Code snippet given by Mozilla Web Docs
-  
- const taskList = props.tasks.map((task) => (
-  <Todo
-    id={task.id}
-    name={task.name}
-    completed={task.completed}
-    key={task.id}
-  />
-));
+  const [tasks, setTasks] = useState(props.tasks);
 
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+
+  function addTask(name) {
+    alert(name);
+  }
 
   return (
     <div className="todoapp stack-large">
       <h1>ToDon't</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
--- frontend/todont/src/components/Form.js
+++ frontend/todont/src/components/Form.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
+// Form component
 function Form(props) {
+  // States
+  const [name, setName] = useState("");
+
+  // submit event
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (name) {
+      props.addTask(name);
+    }
+    setName("");
+  }
+
+  // change event
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
@@ -14,6 +32,8 @@ function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
